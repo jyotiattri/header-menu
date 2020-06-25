772bd64d6b49ec8820fcb51a01f38f8fe7d7fe71
jyoti attri <jyotiatri22@gmail.com>
2020-06-25 13:01:18 +0530

    new components added

---
+++ src/components/TravelEnglandPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Alligators can be found in England's River Valley.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TravelEnglandPage'
+  }
+</script>
+
+<style scoped>
+
+</style>
